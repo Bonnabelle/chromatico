c4c4889282305ba36c72abf45b8dd7164d2f9c69
Bonnabelle <bonnabelleg@gmail.com>
2016-11-15 11:23:24 -0600

    Got quizzes to work correctly. Moving on to saving the statistics to a user.

--- chromatico/signupQuiz.py
+++ chromatico/signupQuiz.py
@@ -20,27 +20,33 @@ class SignupQStartHandler(Utilities):
             response = t.render(username_error="",verify_error="Your usernames must match in order for you to continue!")
             self.response.write(response)
 
+#Initializes a new set of options and their answer, a counter to count questions completed and a variable to track correct answers. to be used throughout the quizzes
 counter = 0
 correct = 0
+options = None
+answer = None
 
 class SignupQuizHandler(Utilities):
     def get(self):
-        #TODO: Make it possible for either this set of two methods to recreate themselves every time a user is redirected
-        #Or make it possible to access the answer from post
+        global counter
+        global options
+        global correct
+        global answer
         options = getOption(1)
         answer = getAnswer(options)
-        global counter
         t = jinja_env.get_template("squiz.html")
-        response = t.render(answer=answer,op1=options[0],op2=options[1],op3=options[2],op4=options[3], counter=counter)
+        response = t.render(answer=answer,op1=options[0],op2=options[1],op3=options[2],op4=options[3], counter=counter,correct=correct)
         self.response.write(response)
 
     def post(self):
         global counter
+        global answer
         global correct
 
         if counter <= 19:
             submitted = self.request.get("option")
-            if self.answer == submitted:
+            #self.response.out.write("User submitted: " + submitted + ", and the answer it was compared to was: " + answer)
+            if answer == submitted: #TODO: Fix this- it's not comparing them correctly
                 counter += 1
                 correct += 1
                 self.redirect("/signupq")
@@ -49,10 +55,12 @@ class SignupQuizHandler(Utilities):
             else:
                 counter += 1
                 self.redirect("/signupq")
-        self.response.out.write("You got " + str(correct) + " out of 20 questions correct. Congratulations!") #TODO: This will redirect the user to their profile page with the results
+        else:
+            self.redirect("/results")
+            
 
 
-
+#For debugging
 class resultsHandler(Utilities):
     def get(self):
-        self.out.response.write(correct)
+        self.response.out.write("You got " + str(correct) + " out of 20 questions correct. Congratulations!") #TODO: This will redirect the user to their profile page with the results
